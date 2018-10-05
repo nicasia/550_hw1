430499739eb6c735127e959adbda20e229b170ae
nbbwang <nbbwang@gmail.com>
2018-10-05 14:09:40 -0700

    io stuff

--- scratch_work.c
+++ scratch_work.c
@@ -5,48 +5,6 @@
 #include <string.h>
 
 
-#define LSH_RL_BUFSIZE 1024
-/**
-   @brief Read a line of input from stdin.
-   @return The line from stdin.
- */
-char *lsh_read_line(void)
-{
-  int bufsize = LSH_RL_BUFSIZE;
-  int position = 0;
-  char *buffer = malloc(sizeof(char) * bufsize);
-  int c;
-
-  if (!buffer) {
-    fprintf(stderr, "lsh: allocation error\n");
-    exit(EXIT_FAILURE);
-  }
-
-  while (1) {
-    // Read a character
-    c = getchar();
-
-    // If we hit EOF, replace it with a null character and return.
-    if (c == EOF || c == '\n') {
-      buffer[position] = '\0';
-      return buffer;
-    } else {
-      buffer[position] = c;
-    }
-    position++;
-
-    // If we have exceeded the buffer, reallocate.
-    if (position >= bufsize) {
-      bufsize += LSH_RL_BUFSIZE;
-      buffer = realloc(buffer, bufsize);
-      if (!buffer) {
-        fprintf(stderr, "lsh: allocation error\n");
-        exit(EXIT_FAILURE);
-      }
-    }
-  }
-}
-
 
 int main(int argc, char **argv)
 {
@@ -69,7 +27,11 @@ int main(int argc, char **argv)
   thing[2] = NULL;
   execvp(cmd, thing);
 
-  lsh_read_line();
+    int a;
+
+    printf("Please input an integer value: ");
+    scanf("%d", &a);
+    printf("You entered: %d\n", a);
 
 
 
