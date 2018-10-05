c8a610ccab50188f7d22df52365df72376e2258f
nbbwang <nbbwang@gmail.com>
2018-10-05 11:23:16 -0700

    basic code

---
+++ scratch_work.c
@@ -0,0 +1,17 @@
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+  // Load config files, if any.
+
+  // Run command loop.
+  fprintf("hi");
+
+  // Perform any shutdown/cleanup.
+
+  return 0;
+}
