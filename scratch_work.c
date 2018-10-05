7fd3b14f99984ca97f5c75fa4deb09b904e62d91
nbbwang <nbbwang@gmail.com>
2018-10-05 14:05:19 -0700

    added temp code

--- scratch_work.c
+++ scratch_work.c
@@ -4,6 +4,50 @@
 #include <stdio.h>
 #include <string.h>
 
+
+#define LSH_RL_BUFSIZE 1024
+/**
+   @brief Read a line of input from stdin.
+   @return The line from stdin.
+ */
+char *lsh_read_line(void)
+{
+  int bufsize = LSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+
+  if (!buffer) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    // Read a character
+    c = getchar();
+
+    // If we hit EOF, replace it with a null character and return.
+    if (c == EOF || c == '\n') {
+      buffer[position] = '\0';
+      return buffer;
+    } else {
+      buffer[position] = c;
+    }
+    position++;
+
+    // If we have exceeded the buffer, reallocate.
+    if (position >= bufsize) {
+      bufsize += LSH_RL_BUFSIZE;
+      buffer = realloc(buffer, bufsize);
+      if (!buffer) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+}
+
+
 int main(int argc, char **argv)
 {
   // Load config files, if any.
@@ -19,7 +63,12 @@ int main(int argc, char **argv)
   // execvp(cmd, thing);
 
   char *cmd = "ls";
-  execvp(argv);
+  char *thing[3];
+  thing[0] = "ls";
+  thing[1] = "-la";
+  thing[2] = NULL;
+  execvp(cmd, thing);
+
 
 
   // Perform any shutdown/cleanup.
