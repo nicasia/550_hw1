e69ee5894812b2881994e496fd07e48a6ffbc622
BercesteDincer <bercestedincer@gmail.com>
2018-10-15 15:51:27 -0700

    Update client.c

--- partb/client.c
+++ partb/client.c
@@ -17,7 +17,7 @@
 
 int main(int argc, char *argv[]) {
     char server_name[SERVER_NAME_LEN_MAX + 1] = { 0 };
-    int server_port, socket_fd;
+    int server_port, socket_fd, ind;
     char file_path[FILE_NAME_LEN_MAX + 1] = { 0 };
     struct hostent *server_host;
     struct sockaddr_in server_address;
@@ -45,6 +45,13 @@ int main(int argc, char *argv[]) {
         scanf("%s", file_path);
     }
 
+/* Get server port from command line arguments or stdin. */
+    ind = argc > 4 ? atoi(argv[4]) : 0;
+    if (!ind) {
+        printf("Enter wait stat: ");
+        scanf("%d", &ind);
+    } 
+
     /* Get server host from server name. */
     server_host = gethostbyname(server_name);
 
@@ -65,20 +72,37 @@ int main(int argc, char *argv[]) {
 		perror("connect");
         exit(1);
 	}
+    
+    //char buff[2000];
+    //int byte_count;
+    //byte_count = recv(socket_fd, buff, sizeof buff, 0);
+    //puts("Received msg from server");
+    //puts(buff);
 
-    if ((send(socket_fd, file_path, sizeof file_path, 0)) == -1) {
+    if(ind == 1){
+	sleep(10);
+    }
+    puts("SENDING filename...\n");
+    puts(file_path);
+    if (send(socket_fd, "a", sizeof file_path, 0) == -1) {
+        perror("sending");
+        exit(1);
+    }
+if (send(socket_fd, ".txt", sizeof file_path, 0) == -1) {
         perror("sending");
         exit(1);
     }
 
     
-
-    char buff[264];
     int byte_count;
-    byte_count = recv(socket_fd, buff, sizeof buff, 0);
-    puts(buff);
-    //sleep(10);
-    //puts("done sleeping");
+    char buff2[2000];
+    byte_count = recv(socket_fd, buff2, sizeof buff2, 0);
+    puts("Received msg from server");
+    puts(buff2);
+
+  
+    sleep(2);
+    puts("done sleeping");
 
     /* TODO: Put server interaction code here. For example, use
      * write(socket_fd,,) and read(socket_fd,,) to send and receive messages
